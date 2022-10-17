9771fd630644de6710d603e178fc2956ee022fa5
Antonino Vespoli <antoninovespoli@eaton.com>
2022-10-17 15:47:49 +0100

    Completed model training code

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,5 +1,9 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 
+from sklearn.ensemble import RandomForestClassifier
+
+from ml.data import process_data
+import pickle
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -18,7 +22,10 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    rfc = RandomForestClassifier(n_jobs=-1)
+
+    return rfc.fit(X_train, y_train)
+
 
 
 def compute_model_metrics(y, preds):
@@ -43,12 +50,65 @@ def compute_model_metrics(y, preds):
     return precision, recall, fbeta
 
 
+def compute_model_metrics_by_slice(model, X, cat_features, label, encoder, lb):
+    """
+    Validates the trained machine learning model using precision, recall, and F1
+    on test dataset slices.
+
+    Inputs
+    ------
+    model:
+        Trained model
+    X : pd.DataFrame
+        Dataframe containing the test dataset
+    cat_features: list[str]
+        List containing the names of the categorical features
+    label : str
+        Name of the label column in `X`.
+    encoder : sklearn.preprocessing._encoders.OneHotEncoder
+        Trained sklearn OneHotEncoder
+    lb : sklearn.preprocessing._label.LabelBinarizer
+        Trained sklearn LabelBinarizer
+
+    Returns
+    -------
+    cat_metrics_list list[Dict]
+    """
+
+    cat_metrics_list = []
+
+    for cat_feature in cat_features:
+        for category in X[cat_feature].unique():
+
+
+            X_cat = X[X[cat_feature] == category]
+
+            x, y, _, _ = process_data(X_cat, categorical_features=cat_features,
+                label=label, training=False, encoder=encoder, lb=lb)
+
+            y_preds = inference(model, x)
+            precision, recall, fbeta = compute_model_metrics(y, y_preds)
+
+            cat_metrics = {}
+            cat_metrics['category_feature'] = cat_feature
+            cat_metrics['category'] = category
+            cat_metrics['precision'] = precision
+            cat_metrics['recall'] = recall
+            cat_metrics['fbeta'] = fbeta
+            cat_metrics['num_elements'] = len(X_cat)
+
+            cat_metrics_list.append(cat_metrics)
+
+
+    return cat_metrics_list
+
+
 def inference(model, X):
     """ Run model inferences and return the predictions.
 
     Inputs
     ------
-    model : ???
+    model : sklearn.ensemble._forest.RandomForestClassifier
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -57,4 +117,35 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    return model.predict(X)
+
+
+def store_model(model, path):
+    """ Store the machine learning model in path.
+
+    Inputs
+    ------
+    model : sklearn.ensemble._forest.RandomForestClassifier
+        Trained machine learning model.
+    path: string
+        The location where to store the model.
+    Returns
+    -------
+    None
+    """
+    pickle.dump(model, open(path, 'wb'))
+
+def load_model(path):
+    """ Load the machine learning model from path
+
+    Inputs
+    ------
+    path: string
+        The location from where to load the model.
+    Returns
+    -------
+    model : sklearn.ensemble._forest.RandomForestClassifier
+        Trained machine learning model.
+    """
+
+    return pickle.load(open(path, 'rb'))
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,10 +1,18 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+from ml.data import process_data
+from ml.model import compute_model_metrics_by_slice, train_model, compute_model_metrics, inference, store_model
 
-# Add the necessary imports for the starter code.
+import pandas as pd
+import csv
 
-# Add code to load in the data.
+INPUT_DATASET_PATH = '../data/census_cleaned.csv'
+MODEL_PATH = '../model/model.pkl'
+MODEL_METRICS_REPORT_PATH = '../model/model_metrics.csv'
+
+# Load the input dataset
+data = pd.read_csv(INPUT_DATASET_PATH)
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -19,10 +27,37 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
+# Preprocess train data
 X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+    train, categorical_features=cat_features, label="salary", training=True)
 
-# Proces the test data with the process_data function.
+# Train model
+model = train_model(X_train, y_train)
 
-# Train and save a model.
+# Save model
+store_model(model, MODEL_PATH)
+
+# Preprocess test data
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False,
+    encoder=encoder, lb=lb)
+
+# Inference with test data
+y_pred = inference(model, X_test)
+
+# Evaluate model performance
+precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
+print("Model performance:\nPrecision:{:.3f}\nRecall:{:.3f}\nfbeta:{:.3f}".format(
+    precision, recall, fbeta))
+
+# Evaluate model performance by slice
+cat_metrics_list = compute_model_metrics_by_slice(model=model, X=test, cat_features=cat_features,
+    label="salary", encoder=encoder, lb=lb)
+
+with open(MODEL_METRICS_REPORT_PATH, "w") as model_report_f:
+    model_report_writer = csv.DictWriter(model_report_f,
+        fieldnames=['category_feature', "category", "precision", "recall", "fbeta", "num_elements"])
+    model_report_writer.writeheader()
+    model_report_writer.writerows(cat_metrics_list)
+
